84bd974196375dc3c0e64dae1158e83a3b139fca
David Simmons <dwsimms925@gmail.com>
2022-06-04 22:51:15 -0400

    switched to mailgun for email handling

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const nodemailer = require('nodemailer')
-const path = require('path')
-const config = require('config')
-const app = express()
+const express = require("express");
+const path = require("path");
+const config = require("config");
+// const sslRedirect = require("heroku-ssl-redirect");
+const app = express();
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+const API_KEY = process.env.API_KEY || config.get("API_KEY");
+const DOMAIN = process.env.DOMAIN || config.get("DOMAIN");
 
-app.post('/email', (req, res) => {
+// app.use(sslRedirect(["production"]));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const mg = new Mailgun(formData);
+const client = mg.client({
+  username: "api",
+  key: API_KEY,
+});
+
+app.post("/email", (req, res) => {
   const output = `
   <p> You have a new contact request </p>
   <h3>Contact Details</h3>
@@ -18,49 +30,44 @@ app.post('/email', (req, res) => {
   </ul>
   <h3>Message</h3>
   <p>${req.body.message}</p>
-  `
+  `;
 
-  let transporter = nodemailer.createTransport({
-    host: config.get('host') || process.env.host,
-    port: config.get('port') || process.env.port,
-    secure: false,
-    auth: {
-      user: config.get('email') || process.env.email,
-      pass: config.get('pass') || process.env.pass,
-    },
-  })
-
-  let mailOptions = {
-    from: '"Simmons Web Dev" <simmonswebdev@gmail.com>',
-    subject: 'New Message from Simmons Web Dev',
-    to: 'simmonswebdev@gmail.com',
-    text: '',
+  const messageData = {
+    from: "Simmons Web Solutions <dwsimms925@gmail.com>",
+    to: "dwsimms925@gmail.com",
+    subject: "You Have a New Request!",
     html: output,
-  }
+  };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error)
-    }
-    console.log('Message sent: %s', info.messageId)
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
-  })
-  return res.status(200).json({ msg: 'Message Sent!', type: 'success' })
-})
+  client.messages
+    .create(DOMAIN, messageData)
+    .then((res) => {
+      // return res.status(200).json({ msg: "Message Sent!", type: "success" });
+      console.log(res);
+    })
+    .catch((err) => {
+      // return res
+      //   .status(400)
+      //   .json({ msg: "Error! Messge Not Sent", type: "error" });
+      console.log(err);
+    });
 
-app.get('/email', (req, res) => {
+  return res.status(200).json({ msg: "Message Sent!", type: "success" });
+});
+
+app.get("/email", (req, res) => {
   return res.status(400).json({
-    msg: 'you must submit a form to access this route!',
-    type: 'error',
-  })
-})
+    msg: "you must submit a form to access this route!",
+    type: "error",
+  });
+});
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Server started on port ${port}`))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
